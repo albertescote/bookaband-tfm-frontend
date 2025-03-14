9a5542142edc7b74a2a406b0798267e9a2ea7cd3
albertescote <albertescote03@gmail.com>
2025-03-14 17:38:10 +0100

    feat: remove console log

--- src/middleware.ts
+++ src/middleware.ts
@@ -56,7 +56,6 @@ export async function middleware(req: NextRequest) {
       `^${langPrefix}${route.replace(/\[.*\]/, '[^/]+')}$`,
     ).test(req.nextUrl.pathname);
   });
-  console.log(protectedRouteFound);
   const isProtectedRoute = !!protectedRouteFound;
 
   if (isProtectedRoute) {
