cff7ccabe4eef00a787edc912928feec2e2bc38a
albertescote <albertescote03@gmail.com>
2025-05-10 15:33:06 +0200

    fix: get user info endpoint

--- bookaband-app/src/service/backend/user/service/user.service.ts
+++ bookaband-app/src/service/backend/user/service/user.service.ts
@@ -1,24 +1,11 @@
 'use server';
 
 import { User } from '@/service/backend/user/domain/user';
-import { getAccessTokenCookie } from '@/service/utils';
-import { decodeJwt } from 'jose';
 import authorizedAxiosInstance from '@/service/authorizedAixosInstance';
 import { withTokenRefreshRetry } from '@/service/backend/auth/service/auth.service';
 
 export async function getUserInfo(): Promise<User | null> {
-  let accessToken = getAccessTokenCookie();
-  if (!accessToken) {
-    console.log('Get user info failed: access token cookie not found');
-    return null;
-  }
-  const decodedJwt = decodeJwt(accessToken);
-  const userId = decodedJwt.sub;
-  if (!userId) {
-    console.log('Invalid access token: missing sub property');
-    return null;
-  }
   return withTokenRefreshRetry(() =>
-    authorizedAxiosInstance.get(`/user/${userId}`).then((res) => res.data),
+    authorizedAxiosInstance.get(`/user`).then((res) => res.data),
   );
 }
